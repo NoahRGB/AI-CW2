e20a1aba01b4e9b90b4dfb3bf06f6ab31fa12a81
Noaaaa <ncox2004@gmail.com>
2025-03-17 13:50:49 +0000

    switch to using URL parameters

--- cheapest_ticket.py
+++ cheapest_ticket.py
@@ -1,4 +1,5 @@
 
+from enum import Enum
 from time import sleep
 
 import requests
@@ -9,51 +10,102 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Keys, ActionChains
 
+class TicketTypes(Enum):
+    SINGLE=1,
+    RETURN=2
+
+class DateTime:
+    months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+    def __init__(self, hour, minute, day=1, month=1, year=2025):
+        self.hour = hour
+        self.minute = minute
+        self.day = day
+        self.month = month
+        self.year = year
+
+        if type(self.month) is str:
+            does_month_exist = DateTime.months.count(self.month.lower()) > 0
+            if does_month_exist:
+                # turn string month into integer month
+                self.month = DateTime.months.index(self.month.lower()) + 1
+        
+        # check all attributes are integers and year is correct length, otherwise reset
+        if not isinstance(self.day, int) or not isinstance(self.month, int) or not isinstance(self.year, int) or len(str(self.year)) != 4:
+            self.reset()
+
+    def reset(self):
+        self.day = 1
+        self.month = 1
+        self.year = 2025
+
+    def __str__(self):
+        return f"{'0' if self.day < 10 else ''}{str(self.day)}{'0' if self.month < 10 else ''}{str(self.month)}{str(self.year)[2:4]}"
+    
+    def get_hour(self):
+        return f"{'0' if self.hour < 10 else ''}{self.hour}"
+    
+    def get_min(self):
+        return f"{'0' if self.minute < 10 else ''}{self.minute}"
+
 class NationalRailScraper:
-    def __init__(self):
+    def __init__(self, origin, destination, date, adults, children):
         options = Options()
         self.browser = Chrome(options=options)
-        self.browser.get("https://www.nationalrail.co.uk/")
         self.actions = ActionChains(self.browser)
-    
+        self.ticket_type = None
+        self.url = (f"https://www.nationalrail.co.uk/journey-planner/?origin={origin}"
+                    + f"&destination={destination}&leavingDate={date}&adults={adults}"
+                    + f"&children={children}&leavingType=departing&extraTime=0")
+
     def __del__(self):
         self.browser.close()
 
     def clear_cookies_popup(self):
-        # looks for cookies accept button until it exists
+        # looks for the cookies accept button until it exists and then clicks it
         cookies_button = self.browser.find_elements(By.ID, "onetrust-accept-btn-handler")
         while len(cookies_button) == 0:
             cookies_button = self.browser.find_elements(By.ID, "onetrust-accept-btn-handler")
-
         cookies_button[0].click()
         sleep(1)
+
+    def set_single_ticket(self, leaving_time):
+        # sets ticket type and URL parameters for a single ticket if no ticket has been set yet
+        if self.ticket_type is None:
+            self.ticket_type = TicketTypes.SINGLE
+            self.url += f"&type=single&leavingHour={leaving_time.get_hour()}&leavingMin={leaving_time.get_min()}"
+        else:
+            return False
     
-    def launch_journey_box(self):
-        # clicks the button to launch the journey planner on the homepage
-        initial_button = self.browser.find_element(By.CSS_SELECTOR, "button[data-testid='jp-preview-btn']")
-        initial_button.click()  
-        sleep(1)
-    
-    def set_stations(self, origin, destination):
-        # adds the origin and destination stations to the relevant boxes in the journey planner box
-        origin_input = self.browser.find_element(By.ID, "jp-origin")
-        destination_input = self.browser.find_element(By.ID, "jp-destination")
+    def set_return_ticket(self, leaving_time, return_date):
+        # sets ticket type and URL parameters for a return ticket if no ticket has been set yet
+        if self.ticket_type is None:
+            self.ticket_type = TicketTypes.RETURN
+            self.url += (f"&type=return&returnType=departing&leavingHour={leaving_time.get_hour()}"
+                         + f"&leavingMin={leaving_time.get_min()}&returnDate={str(return_date)}"
+                         + f"&returnHour={return_date.get_hour()}&returnMin={return_date.get_min()}")
+        else:
+            return False
+        
+    def launch_scraper(self):
+        self.browser.get(self.url)
 
-        origin_input.send_keys(origin)
-        self.press_enter();
-        destination_input.send_keys(destination)
 
-        sleep(1)
 
-    def press_enter(self):
-        self.actions.key_down(Keys.ENTER).perform()
- 
+# gather this information from chatbot
+ticket = "single"
+leaving_date = DateTime(12, 45, 19, 3, 2025)
+return_date = DateTime(13, 45, 21, 3, 2025)
+origin = "NRW"
+destination = "IPS"
+adults = 2
+children = 2
 
-scraper = NationalRailScraper()
-
-scraper.clear_cookies_popup()
-scraper.launch_journey_box()
-scraper.set_stations("NRW", "IPS")
-
+# use it with scraper to retrieve journey times
+scraper = NationalRailScraper(origin, destination, leaving_date, adults, children)
+# scraper.set_single_ticket(leaving_date)
+scraper.set_return_ticket(leaving_date, return_date)
+scraper.launch_scraper()
+scraper.clear_cookies_popup();
 
 input()
