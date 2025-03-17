2bcafd663119b9cb4c1c4b2bc6ea8b20b3b25647
Noaaaa <ncox2004@gmail.com>
2025-03-17 22:24:45 +0000

    basic cheapest ticket

--- cheapest_ticket.py
+++ cheapest_ticket.py
@@ -18,8 +18,8 @@ class DateTime:
     months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 
     def __init__(self, hour, minute, day=1, month=1, year=2025):
-        self.hour = hour
-        self.minute = minute
+        self.__hour = hour
+        self.__minute = minute
         self.day = day
         self.month = month
         self.year = year
@@ -43,10 +43,10 @@ class DateTime:
         return f"{'0' if self.day < 10 else ''}{str(self.day)}{'0' if self.month < 10 else ''}{str(self.month)}{str(self.year)[2:4]}"
     
     def get_hour(self):
-        return f"{'0' if self.hour < 10 else ''}{self.hour}"
+        return f"{'0' if self.__hour < 10 else ''}{self.__hour}"
     
     def get_min(self):
-        return f"{'0' if self.minute < 10 else ''}{self.minute}"
+        return f"{'0' if self.__minute < 10 else ''}{self.__minute}"
 
 class NationalRailScraper:
     # queries the national rail website (https://www.nationalrail.co.uk/) for the
@@ -90,35 +90,71 @@ class NationalRailScraper:
         else:
             return False
         
+    def get_info_on_ticket(self, ticket_number):
+        # returns an object with departure/arrival time, length and price for a ticket given
+        # by the number that it appears in the ticket list
+        # MUST BE on the ticket list page
+        ticket_info = { "departure_time": None, "arrival_time": None, "length": None, "price": None}
+        ticket_container = self.browser.find_element(By.CSS_SELECTOR, f"div[data-testid='card-result-card-outward-{ticket_number}'")
+        times = ticket_container.find_elements(By.TAG_NAME, "time")
+        ticket_info["departure_time"] = times[0].text
+        ticket_info["arrival_time"] = times[1].text
+        ticket_info["length"] = times[2].text[:-1] # remove redundant comma
+        ticket_info["price"] = self.get_ticket_price(ticket_number)
+        return ticket_info
+    
+    def get_ticket_price(self, ticket_number):
+        # finds the price for a ticket given by the number that it appears in the ticket list
+        # MUST BE on the ticket list page
+        price_holder = self.browser.find_element(By.ID, f"result-card-price-outward-{ticket_number}")
+        price = price_holder.find_element(By.TAG_NAME, "div").find_elements(By.TAG_NAME, "span")[1].text
+        return price    
+        
+    def get_cheapest_listed(self):
+        # returns the ticket info using get_info_on_ticket() for the cheapest ticket in the list
+        # MUST BE on the ticket list page
+        cheapest_ticket = { "price": "999" }
+        result_num = 0
+        should_continue = len(self.browser.find_elements(By.ID, f"result-card-price-outward-{result_num}")) > 0
+        while should_continue:
+            price = self.get_ticket_price(result_num)
+            if float(price[1:]) < float(cheapest_ticket["price"][1:]):
+                cheapest_ticket = self.get_info_on_ticket(result_num)
+
+            result_num += 1
+            should_continue = len(self.browser.find_elements(By.ID, f"result-card-price-outward-{result_num}")) > 0
+        return cheapest_ticket
+
     def launch_scraper(self):
         self.browser.get(self.url)
 
 
 
+
 with open("station_list.pickle", "rb") as file:
     station_dict = pickle.load(file)
-print(station_dict)
 
 # ========================================================
 # gather this information from the chatbot (somehow)
 ticket = TicketTypes.RETURN
-leaving_date = DateTime(12, 45, 19, 3, 2025)
-return_date = DateTime(13, 45, 21, 3, 2025)
+leaving_date = DateTime(12, 30, 19, 3, 2025)
+# return_date = DateTime(13, 45, 21, 3, 2025)
 origin = "NRW"
 destination = "IPS"
-adults = 2
-children = 2
+adults = 1
+children = 1
 # ========================================================
-
-
 
 
 # ================================================================================
 # use the gathered information with a web scraper to retrieve journey times
 scraper = NationalRailScraper(origin, destination, leaving_date, adults, children)
-scraper.set_return_ticket(leaving_date, return_date)
+scraper.set_single_ticket(leaving_date)
+# scraper.set_return_ticket(leaving_date, return_date)
 scraper.launch_scraper()
-scraper.clear_cookies_popup();
+scraper.clear_cookies_popup()
+print(scraper.get_cheapest_listed())
+# print(scraper.get_info_on_ticket(1))
 # ================================================================================
 
 
